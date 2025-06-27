3be010bd93efb3c3754a2d444bdef85337c07e13
Gunter Garate <gu.garate@duocuc.cl>
2025-06-27 02:11:44 -0400

    Delete Ecomarket/Venta directory

--- Ecomarket/Venta/src/main/java/com/Ecomarket/Venta/repository/DetalleVentaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Ecomarket.Venta.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Ecomarket.Venta.model.DetalleVenta;
-
-public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
-
-}
